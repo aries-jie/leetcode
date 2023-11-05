18f9bfb91be0321c4bb37cf7f776789f060633ac
xiaojie.huang <302785475@qq.com>
2023-11-05 10:02:46 +0800

    leetcode 19.删除链表的倒数第 N 个节点 -- 添加栈的解法

--- src/main/java/org/example/Solution19.java
+++ src/main/java/org/example/Solution19.java
@@ -1,7 +1,9 @@
 package org.example;
 
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 
 /**
@@ -30,6 +32,29 @@ public class Solution19 {
         } else {
             return delNode.next;
         }
+    }
+
+    /**
+     * 栈的解法
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0, head);
+        Deque<ListNode> stack = new LinkedList<ListNode>();
+        ListNode cur = dummy;
+        while (cur != null) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+        for (int i = 0; i < n; ++i) {
+            stack.pop();
+        }
+        ListNode prev = stack.peek();
+        prev.next = prev.next.next;
+        ListNode ans = dummy.next;
+        return ans;
     }
 
     public class ListNode {
