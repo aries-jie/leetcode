dd828c4b9a5d643b7cff357b29186eb09d6592ee
Aries <302785475@qq.com>
2023-12-22 21:59:34 +0800

    leetcode 226.翻转二叉树

---
+++ src/main/java/org/example/Solution226.java
@@ -0,0 +1,45 @@
+package org.example;
+
+
+/**
+ * 翻转二叉树
+ * @author xiaojie.huang
+ * @since 2023-12-22
+ */
+public class Solution226 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root != null) {
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
