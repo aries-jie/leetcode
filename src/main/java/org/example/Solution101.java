4d2c0656c4bb4d3dc775d8d05c9a5394ad4789a8
Aries <302785475@qq.com>
2023-12-22 22:10:54 +0800

    leetcode 101.对称二叉树

---
+++ src/main/java/org/example/Solution101.java
@@ -0,0 +1,48 @@
+package org.example;
+
+
+/**
+ * 对称二叉树
+ * @author xiaojie.huang
+ * @since 2023-12-22
+ */
+public class Solution101 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return compare(root.left, root.right);
+    }
+
+    public boolean compare(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return compare(left.left, right.right) && compare(left.right, right.left);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
