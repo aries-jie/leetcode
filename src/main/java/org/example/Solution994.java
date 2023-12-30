d38a1de4fc556fafbc8a5c27d729e8f3dd6e44c6
Aries <302785475@qq.com>
2023-12-30 08:03:52 +0800

    leetcode 994.新增官方解法-广度优先遍历

--- src/main/java/org/example/Solution994.java
+++ src/main/java/org/example/Solution994.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
 /**
  * 腐烂的橘子
  *
@@ -103,7 +108,56 @@ public class Solution994 {
         int[][] a = {
                 {0, 2}
         };
-        System.out.println(new Solution994().orangesRotting(a));;
+        System.out.println(new Solution994().orangesRotting2(a));;
     }
+
+
+
+    int[] dr = new int[]{-1, 0, 1, 0};
+    int[] dc = new int[]{0, -1, 0, 1};
+
+    /**
+     * 官方解法
+     * 广度优先遍历
+     */
+    public int orangesRotting2(int[][] grid) {
+        int R = grid.length, C = grid[0].length;
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        Map<Integer, Integer> depth = new HashMap<Integer, Integer>();
+        for (int r = 0; r < R; ++r) {
+            for (int c = 0; c < C; ++c) {
+                if (grid[r][c] == 2) {
+                    int code = r * C + c;
+                    queue.add(code);
+                    depth.put(code, 0);
+                }
+            }
+        }
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int code = queue.remove();
+            int r = code / C, c = code % C;
+            for (int k = 0; k < 4; ++k) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+                if (0 <= nr && nr < R && 0 <= nc && nc < C && grid[nr][nc] == 1) {
+                    grid[nr][nc] = 2;
+                    int ncode = nr * C + nc;
+                    queue.add(ncode);
+                    depth.put(ncode, depth.get(code) + 1);
+                    ans = depth.get(ncode);
+                }
+            }
+        }
+        for (int[] row: grid) {
+            for (int v: row) {
+                if (v == 1) {
+                    return -1;
+                }
+            }
+        }
+        return ans;
+    }
+
 
 }
