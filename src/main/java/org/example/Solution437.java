a57d9fd01efbedd1243452ed87fbda94a73a09f3
Aries <302785475@qq.com>
2024-01-24 22:58:40 +0800

    leetcode 437.路径总和 III

--- src/main/java/org/example/Solution437.java
+++ src/main/java/org/example/Solution437.java
@@ -1,7 +1,9 @@
 package org.example;
 
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 /**
  * 路径总和 III
@@ -68,7 +70,7 @@ public class Solution437 {
     }
 
     public static void main(String[] args) {
-        TreeNode a1 = new TreeNode(1000000000);
+        /*TreeNode a1 = new TreeNode(1000000000);
         TreeNode a2 = new TreeNode(1000000000);
         TreeNode a3 = new TreeNode(294967296);
         TreeNode a4 = new TreeNode(1000000000);
@@ -79,7 +81,58 @@ public class Solution437 {
         a3.left = a4;
         a4.left = a5;
         a5.left = a6;
-        System.out.println(new Solution437().pathSum(a1, 0));
+        System.out.println(new Solution437().pathSum(a1, 0));*/
+
+        TreeNode a1 = new TreeNode(10);
+        TreeNode a2 = new TreeNode(5);
+        TreeNode a3 = new TreeNode(-3);
+        TreeNode a4 = new TreeNode(3);
+        TreeNode a5 = new TreeNode(2);
+        TreeNode a6 = new TreeNode(11);
+        TreeNode a7 = new TreeNode(3);
+        TreeNode a8 = new TreeNode(-2);
+        TreeNode a9 = new TreeNode(1);
+
+        a1.left = a2;
+        a1.right = a3;
+
+        a2.left = a4;
+        a2.right = a5;
+
+        a3.right= a6;
+
+        a4.left = a7;
+        a4.right = a8;
+
+        a5.right = a9;
+
+        System.out.println(new Solution437().pathSum2(a1, 8));
+    }
+
+    /**
+     * 前缀和
+     */
+    public int pathSum2(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefix = new HashMap<Long, Integer>();
+        prefix.put(0L, 1);
+        return dfs(root, prefix, 0, targetSum);
+    }
+
+    public int dfs(TreeNode root, Map<Long, Integer> prefix, long curr, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = 0;
+        curr += root.val;
+
+        ret = prefix.getOrDefault(curr - targetSum, 0);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
+        ret += dfs(root.left, prefix, curr, targetSum);
+        ret += dfs(root.right, prefix, curr, targetSum);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) - 1);
+
+        return ret;
     }
 
 }
